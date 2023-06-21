bb3dc2e7b08560fb0e20b063a8e8ee2ac1e48871
Paul Nosenko <pavelgadzenko@gmail.com>
2023-06-21 18:48:49 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SearchForm from "./components/SearchForm/SearchForm";
+import Hero from "./components/Hero/Hero";
+import List from "./components/List/List";
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,14 @@
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import styles from './SearchForm.module.scss';
+
+const SearchForm = () => {
+    return (
+        <form className={styles.searchForm}>
+            <TextInput placeholder="Search…"/>
+            <Button text="Search" />
+        </form>
+    );
+  };
+
+  export default SearchForm;
