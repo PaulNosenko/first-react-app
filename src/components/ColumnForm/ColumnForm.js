2abec9f44c0462d1a080cf78e37ab9c8d6a984f1
Paul Nosenko <pavelgadzenko@gmail.com>
2023-06-22 19:16:12 +0200

    [master] - Added responsive styling for ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import styles from './ColumnForm.module.scss';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = (props) => {
     const [title, setTitle] = useState('');
 
     const [icon, setIcon] = useState('');
-    
+
     const handleSubmit = e => {
         e.preventDefault();
         props.action({ title: title, icon: icon });
@@ -14,10 +16,16 @@ const ColumnForm = (props) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-            Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-            <button>Add column</button>
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            <fieldset className={styles.fieldset}>
+                <label className={styles.label}>Title: </label>
+                <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+            </fieldset>
+            <fieldset className={styles.fieldset}>
+                <label className={styles.label}>Icon: </label>
+                <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
+            </fieldset>
+            <Button>Add column</Button>
         </form>
     );
 };
