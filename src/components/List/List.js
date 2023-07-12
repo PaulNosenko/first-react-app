24f9dc4ed7c695f61c2f40339a8dc2687506aecb
Paul Nosenko <pavelgadzenko@gmail.com>
2023-07-12 20:26:12 +0200

    [master] - Fixed issue with not being able to add new column to specific list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -23,7 +23,7 @@ const List = () => {
                 {...column}  />
             )}
           </section>
-          <ColumnForm />
+          <ColumnForm listId={listId} />
         </div>
       );
 }
