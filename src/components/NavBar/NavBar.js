1b16acbd8e5a58c8c060d9d8a9fab8bd17a4b945
Paul Nosenko <pavelgadzenko@gmail.com>
2023-07-12 18:29:06 +0200

    [master] - Added active link styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import Container from "../Container/Container";
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -10,9 +11,15 @@ const NavBar = () => {
                         <a href="/"><i className={'fa fa-tasks'}></i></a>
                     </div>
                     <ul className={styles.navLinks}>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/favorite">Favorite</a></li>
-                        <li><a href="/about">About</a></li>
+                        <li>
+                            <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink>
+                        </li>
                     </ul>
                 </div>
             </Container>
